addcb07060e6c672f05e5fc1d4636b192ea4758e
James Mead <james.mead+rpf@gofreerange.com>
2025-02-19 16:23:00 +0000

    Export manualUpdateProject
    So we can trigger the saving of a project from outside the GUI
    component.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen
 import LocalesReducer, {localesInitialState, initLocale} from './reducers/locales';
 import {ScratchPaintReducer} from 'scratch-paint';
 import {setFullScreen, setPlayer} from './reducers/mode';
-import {remixProject} from './reducers/project-state';
+import {manualUpdateProject, remixProject} from './reducers/project-state';
 import {setAppElement} from 'react-modal';
 
 const guiReducers = {
@@ -25,6 +25,7 @@ export {
     initFullScreen,
     initLocale,
     localesInitialState,
+    manualUpdateProject,
     remixProject,
     setFullScreen,
     setPlayer
